486e9ffd1277f9fde3710096dba201bdecc44657
Asep Mulyana <asmul.dev@gmail.com>
2026-03-08 06:19:56 +0700

    fix

--- lib/app/modules/absensi_siswa/bindings/absensi_siswa_binding.dart
+++ lib/app/modules/absensi_siswa/bindings/absensi_siswa_binding.dart
@@ -1,12 +1,17 @@
 import 'package:get/get.dart';
 import 'package:epesantren_mob/app/api/santri/santri_repository.dart';
+import 'package:epesantren_mob/app/api/orangtua/orangtua_api.dart';
+import 'package:epesantren_mob/app/api/orangtua/orangtua_repository.dart';
 import '../controllers/absensi_siswa_controller.dart';
 
 class AbsensiSiswaBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<AbsensiSiswaController>(
-      () => AbsensiSiswaController(SantriRepository()),
+      () => AbsensiSiswaController(
+        SantriRepository(),
+        OrangtuaRepository(OrangtuaApi()),
+      ),
     );
   }
 }
